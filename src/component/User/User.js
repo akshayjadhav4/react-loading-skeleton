71df68451e723dfa70590e53ddd08c49bdd8a7d5
AKSHAY <akshayjadhav461999@gmail.com>
2020-12-17 15:12:10 +0530

    fetching data

--- src/component/User/User.js
+++ src/component/User/User.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function User() {
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+      const data = await res.json();
+      setProfile(data);
+    }, 5000);
+  }, []);
   return (
     <div className="User">
       <h2>User Profile</h2>
+      {profile && (
+        <div className="user__profile">
+          <h3>{profile.username}</h3>
+          <p>{profile.email}</p>
+          <a href={profile.website}>{profile.website}</a>
+        </div>
+      )}
+      {!profile && <div>Loading...</div>}
     </div>
   );
 }
