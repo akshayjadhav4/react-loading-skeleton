21102b7cbf51cd4597a85aa42f59b8940048ca1e
AKSHAY <akshayjadhav461999@gmail.com>
2020-12-17 14:55:20 +0530

    layout setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import Articles from "./component/Articles/Articles";
+import User from "./component/User/User";
 
 function App() {
   return (
     <div className="app">
-      <header className="app__header">React Skeleton Learn</header>
+      <header className="app__header">
+        <h1>React Skeleton Learn</h1>
+      </header>
+      <div className="app__content">
+        <Articles />
+        <User />
+      </div>
     </div>
   );
 }
---
+++ src/component/User/User.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function User() {
+  return (
+    <div className="User">
+      <h2>User Profile</h2>
+    </div>
+  );
+}
+
+export default User;
